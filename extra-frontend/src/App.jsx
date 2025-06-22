03e724e75b0c30b93f2f2e607c1d98847214d7d5
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-06-23 01:31:01 +0800

    Created container elements

--- extra-frontend/src/App.jsx
+++ extra-frontend/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(${props => props.$cols || 12 }, 1fr);
+    gap: 20px;
+    margin: 10px auto;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`
+
+export const GridItem = styled.div`
+    grid-column: span ${props => props.$cols || 12 } / auto; 
+    grid-row: span ${props => props.$rows || 1 } / auto;
+
+    display: grid;
+    grid-template-columns: repeat(${props => props.$cols || 12 }, 1fr);
+    gap: 20px;
+
+`
+    
+export const BorderedGridItem = styled(GridItem)`
+    padding: var(--padding);
+    border: var(--wireframe);
+    border-radius: var(--radius);
+    box-shadow: var(--box-shadow);
+`
+
+function App(){
+    return(
+        <>
+            <GridContainer>
+                <GridItem>
+                    <h1>Header</h1>
+                </GridItem>
+
+                <BorderedGridItem $cols = {6}>
+                    a
+                </BorderedGridItem>
+
+                <BorderedGridItem $cols = {6} $rows = {2}>
+                    b
+                </BorderedGridItem>
+
+                <BorderedGridItem $cols = {3}>
+                    c
+                </BorderedGridItem>
+
+                <BorderedGridItem $cols = {3}>
+                    d
+                </BorderedGridItem>
+
+                <GridItem>
+                    <h1>Header</h1>
+                </GridItem>
+
+                <GridItem $cols = {12}>
+                
+                    <BorderedGridItem $cols = {1} >
+                        e
+                    </BorderedGridItem>
+                
+                    <BorderedGridItem $cols = {6}>
+                        f
+                    </BorderedGridItem>
+
+                    <BorderedGridItem $cols = {2}>
+                        g
+                    </BorderedGridItem>
+                    <BorderedGridItem $cols = {2}>
+                        h
+                    </BorderedGridItem>
+                    <BorderedGridItem $cols = {1}>
+                        i
+                    </BorderedGridItem>
+                
+                </GridItem>
+            </GridContainer>
+        </>
+    );
 }
 
 export default App
