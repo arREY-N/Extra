e9f0be3c67b844dd8e6e802fbe64693861a89997
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-06-23 23:13:10 +0800

    Created graph container components

---
+++ extra-frontend/src/Visual.jsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const ChartStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .info{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .chart > img{
+        width: 100%;
+    }
+`
+export const LegendStyle = styled.div`
+    margin: 2px 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+
+    .legendIcon{
+        flex-basis: 10px;
+        flex-shrink: 0;
+        flex-grow: 0;
+        height: 10px;
+        border-radius: 100px;
+        margin-right: 5px;
+        border: var(--wireframe);
+    }
+` 
+
+export function LineChart(props){
+    return(
+        <>
+            <ChartStyle>
+                <div className="chart">
+                    <img src="https://placehold.co/550x250" alt="" />
+                </div>
+            </ChartStyle>
+        </>
+    );
+}
+
+export function PieChart(props){
+    return (
+        <>
+            <ChartStyle>
+                <div className="chart">
+                    <img src="https://placehold.co/250" alt="" />
+                </div>
+
+                <div className="info">
+                    <Legend 
+                        $color = 'red'
+                        $text = 'Category 1'/>
+                    
+                    <Legend 
+                        $color = 'green'
+                        $text = 'Category 2'/>
+
+                    <Legend 
+                        $text = 'Category 3'/>
+                </div>
+            </ChartStyle>
+        </>
+    );
+}
+
+export function Legend(props){
+    return(
+        <>
+            <LegendStyle>
+                <div 
+                    className="legendIcon" 
+                    style={{backgroundColor: props.$color}}
+                />
+
+                <p>{props.$text}</p>
+            </LegendStyle>
+        </>
+    );
+}
