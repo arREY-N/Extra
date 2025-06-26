2aadfe972cf7545a92211cabd9c3662e0a8967f0
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-06-26 22:29:54 +0800

    Revised info pill layouts

--- extra-frontend/src/Pill.jsx
+++ extra-frontend/src/Pill.jsx
@@ -2,28 +2,18 @@ import styled from "styled-components";
 
 export const PillStyle = styled.div`
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    color: var(${props => props.$textColor || '--black' });
+    flex-direction: column;
+    align-items: start;
     overflow: hidden;
     white-space: nowrap;
-
-    .pillIcon{
-        flex: 1fr;
-        margin-right: 10px;
-    }
+    padding: 10px;
 
     .pillInfo{
-        flex: 2;
+
     }
 
     .pillIcon > img{
         border-radius: 50%;
-    }
-
-    .value{
-        font-size: ${props => props.$fontSize || '1.5'}rem;
-        font-weight: bold;
     }
 `
 
@@ -38,15 +28,14 @@ export function Pill(props){
                     <img src={props.$img} alt="" />
                 </div>
                 <div className="pillInfo">        
-                    <div className="title">
-                        {props.$title}
-                    </div>
                     <div className="value">
-                        {props.$value}
+                        <h1>{props.$value}</h1>
+                    </div>
+                    <div className="title">
+                        <h3>{props.$title}</h3>
                     </div>
                 </div>
             </PillStyle>
         </>
     );
 }
-
