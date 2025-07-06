763fb23c738d0183c5fafad5224be223b11f3509
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-07-06 16:37:11 +0800

    Added transaction form

--- extra-frontend/src/components/Transaction.jsx
+++ extra-frontend/src/components/Transaction.jsx
@@ -1,5 +1,7 @@
+import { GridItem, BorderedGridItem } from './Containers';
+import '../styles/Transaction.css';
 import styled from "styled-components";
-import { GridItem } from "./Containers";
+import { useState } from "react";
 
 export const Row = styled.div`
     margin: 0 10px;
@@ -85,6 +87,114 @@ export function Entry(props){
                    {props.$flow == 1 ? '-' : '+' } P {props.$amount.toFixed(2)}
                 </div>
             </Row>
+        </>
+    );
+}
+
+export function TransactionForm(){
+    const [flow, setFlow] = useState('Income');
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+    const [amount, setAmount] = useState(undefined);
+    const [date, setDate] = useState(new Date().toISOString().slice(0,10));
+
+    const categories = ["Wants", "Needs", "Savings"];
+    return(
+        <>
+            <BorderedGridItem className='transaction-form' $spanCols = {12} $cols = {1}>
+                <GridItem $spanCols = {1} style={{marginTop: '10px', marginLeft: '10px'}}>
+                    <h2>Add New Transaction</h2>
+                </GridItem>
+            
+                <form>
+                    <fieldset>
+                        <div className='radio-group'>
+                            <a
+                                className = {flow === 'Income' ? 'active-radio' : 'radio'}
+                                onClick={() => setFlow('Income')}>
+                                    Income
+                            </a>
+                            <a 
+                                className={flow === 'Expense' ? 'active-radio' : 'radio'} 
+                                onClick={() => setFlow('Expense')}>
+                                Expense
+                            </a>
+                        </div>
+
+                        {
+                            flow === 'Expense' && (
+                                <>
+                                    <label className='form-label' htmlFor="transactionCategory">
+                                        Category
+                                    </label>
+
+                                    <select 
+                                        className='form-input'
+                                        name = "category" 
+                                        id = "transactionCategory"
+                                        value = {category}
+                                        onChange = {(e) => setCategory(e.target.value)}>            
+
+                                        <option value = '' disabled> Set Category </option>
+                                        {
+                                            categories.map((cat) => (
+                                                <option key = {cat} value = {cat}> 
+                                                    {cat} 
+                                                </option> 
+                                            ))
+                                        }
+                                    </select>
+                                </>
+                            )
+                        }
+
+                        <label className='form-label' htmlFor="transactionItem">
+                            Transaction Item
+                        </label>
+                        <input
+                            className='form-input' 
+                            type = 'text'
+                            id = 'name'
+                            name = 'name'
+                            value = {name}
+                            placeholder='Enter transaction item'
+                            onChange={(e) => setName(e.target.value)}/>
+
+                        <label className='form-label' htmlFor = "transactionPrice">
+                            Amount
+                        </label>
+                        <input
+                            className='form-input'
+                            type = 'number'
+                            id = 'amount'
+                            name = 'amount'
+                            value = {amount}
+                            placeholder='Enter amount'
+                            onChange = {(e) => setAmount(e.target.value)}/> 
+
+                        <label className='form-label' htmlFor = 'transactionDate'>
+                            Date
+                        </label>
+                        <input
+                            className='form-input'
+                            type = 'date'
+                            id = 'name'
+                            name = 'date'
+                            value = {date}
+                            onChange = {(e) => setDate(e.target.value)}/>
+
+                        <button className='transaction-button' type = 'submit'>
+                            Add Transaction 
+                        </button>
+                    </fieldset>
+                </form>
+            </BorderedGridItem>
+
+            {flow}
+            {name}
+            {amount}
+            {category}
+
         </>
     );
 }
